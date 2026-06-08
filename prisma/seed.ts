97360440a91376972562e09b40852aff2750fc31
vladkhmurov675-eng <vladkhmurov675@gmail.com>
2026-06-08 10:18:47 +0500

    deploy

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,11 +3,11 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 import bcrypt from 'bcryptjs';
 
-import Prisma from '@prisma/client';
+const { PrismaClient } = await import('@prisma/client');
 
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
-const prisma = new Prisma.PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter })
 
 async function main() {
   console.log('Seeding database...');
