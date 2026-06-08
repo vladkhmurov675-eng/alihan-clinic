29385b83e0db00b5750fdbb8cd452ea0e01925ea
vladkhmurov675-eng <vladkhmurov675@gmail.com>
2026-06-08 11:37:15 +0500

    deploy

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,3 +1,4 @@
+'use server'
 import 'server-only';
 import bcrypt from 'bcrypt';
 import { prisma } from '../db';
--- src/db.ts
+++ src/db.ts
@@ -1,3 +1,5 @@
+
+'use server'
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
